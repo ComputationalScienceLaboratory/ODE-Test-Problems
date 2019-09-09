8b2f23111b96eaca1b125da3b76623f7b99675c0
Steven Roberts <sroberts994@gmail.com>
2019-09-08 22:08:43 -0400

    Get PR initial condition from phi and extend tspan

--- src/+otp/+protherorobinson/+presets/Canonical.m
+++ src/+otp/+protherorobinson/+presets/Canonical.m
@@ -6,16 +6,16 @@ classdef Canonical < otp.protherorobinson.ProtheroRobinsonProblem
             end
             if nargin < 2
                 phi = @sin;
-            end
-            if nargin < 3
                 dphi = @cos;
             end
             
             params.lambda = lambda;
             params.phi = phi;
             params.dphi = dphi;
+            tspan = [0, 10];
+            y0 = phi(tspan(1));
             
-            obj = obj@otp.protherorobinson.ProtheroRobinsonProblem([0, 1], ones(size(lambda, 1), 1), params);
+            obj = obj@otp.protherorobinson.ProtheroRobinsonProblem(tspan, y0, params);
         end
         
     end
