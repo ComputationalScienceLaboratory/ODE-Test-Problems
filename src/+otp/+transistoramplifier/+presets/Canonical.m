13ce5d95e145e82e9e3ccce14082a78e306267a0
Andrey Popov <apopov@vt.edu>
2020-02-25 14:42:35 -0500

    QoL fixes

--- src/+otp/+transistoramplifier/+presets/Canonical.m
+++ src/+otp/+transistoramplifier/+presets/Canonical.m
@@ -1,9 +1,7 @@
 classdef Canonical < otp.transistoramplifier.TransistorAmplifierProblem
     methods
-        function obj = Canonical()
-
-            tspan = [0., 0.2];
-
+        function obj = Canonical
+            tspan = [0.0, 0.2];
 
             params.C = (1:5)*1e-6;
             params.R = [1000, 9000*ones(1,9)];
@@ -14,10 +12,9 @@ classdef Canonical < otp.transistoramplifier.TransistorAmplifierProblem
 
             y02 = params.Ub/(params.R(3)/params.R(2) +1);
             y05 = params.Ub/(params.R(7)/params.R(6) +1);
-            y0 = [0;y02;y02;params.Ub;y05;y05;params.Ub;0];
+            y0 = [0; y02; y02; params.Ub; y05; y05; params.Ub; 0];
 
             obj = obj@otp.transistoramplifier.TransistorAmplifierProblem(tspan, y0, params);            
         end
     end
 end
-
