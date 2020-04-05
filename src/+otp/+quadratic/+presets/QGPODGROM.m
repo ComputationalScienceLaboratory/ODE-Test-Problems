bd48ded939664061b5b63e957b865e30dbd844a7
Andrey A Popov <apopov@vt.edu>
2020-04-05 11:30:30 -0400

    Added option to change basis size in quadratic rom

--- src/+otp/+quadratic/+presets/QGPODGROM.m
+++ src/+otp/+quadratic/+presets/QGPODGROM.m
@@ -1,15 +1,19 @@
 classdef QGPODGROM < otp.quadratic.QuadraticProblem
 
     methods
-        function obj = QGPODGROM
+        function obj = QGPODGROM(r)
             tspan = [0, 80/(365.25*20.12)];
+            
+            if nargin < 1
+                r = 100;
+            end   
             
             s = load('PMISr100sp.mat');
             
-            params.a = double(s.b);
-            params.B = double(s.A);
-            params.C = double(s.B);
-            y0 = double(s.a0);
+            params.a = double(s.b(1:r));
+            params.B = double(s.A(1:r, 1:r));
+            params.C = double(s.B(1:r, 1:r, 1:r));
+            y0 = double(s.a0(1:r));
             
             obj = obj@otp.quadratic.QuadraticProblem(tspan, y0, params);
         end
