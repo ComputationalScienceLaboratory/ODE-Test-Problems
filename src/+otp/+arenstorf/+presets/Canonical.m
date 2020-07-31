3a620a06cf09c1879a92247d18068d071c147891
Arash Sarshar <sarshar@vt.edu>
2020-07-30 21:28:38 -0400

    fixed pull req comments

--- src/+otp/+arenstorf/+presets/Canonical.m
+++ src/+otp/+arenstorf/+presets/Canonical.m
@@ -1,13 +1,19 @@
-classdef Canonical < otp.arenstorf.ArenstorfProblem
+classdef Canonical < otp.arenstorf.Arenstorf
     methods
-        function obj = Canonical
-            params.m1 = 0.012277471;
-            params.m2 = 1-0.012277471;
-
-            y0 = [0.994; 0; 0; -2.00158510637908252240537862224];
-            tspan = [0,20];
+        function obj = Canonical(varargin)
+                 
+            p = inputParser;
+            addParameter(p, 'm1', 0.012277471, @isscalar);
+            parse(p, varargin{:});
+            s = p.Results;
             
-            obj = obj@otp.arenstorf.ArenstorfProblem(tspan, y0, params);
+            params.m1 = s.m1;
+            params.m2 = 1-params.m1;
+                
+            y0 = [0.994; 0; 0; -2.00158510637908252240537862224];
+            tspan = [0,17.0652165601579625588917206249];
+            
+            obj = obj@otp.arenstorf.Arenstorf(tspan, y0, params);
         end
 
     end
