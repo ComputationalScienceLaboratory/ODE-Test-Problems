064fdb62665730fe96104d1409d89ed71a89f978
Steven Roberts <sroberts994@gmail.com>
2020-11-02 21:58:43 -0500

    Optimize the Jacobian

--- src/+otp/+pendulum/jac.m
+++ src/+otp/+pendulum/jac.m
@@ -1,16 +1,12 @@
 function jac = jac(~, y, lengths, cumulativeMasses, g, scaledMasses)
+
 n = numel(lengths);
 angles = y(1:n);
 velocities = y(n+1: end).';
-dadv = -2 * scaledMasses .* velocities .* sin(angles - angles.');
-dada1 = scaledMasses .* (velocities.^2) .* cos(angles - angles.');
-dada1 = dada1 - diag(diag(dada1));
-dada1 = dada1 - diag(sum(dada1,2));
-dada = dada1 + diag(cumulativeMasses * -g .* lengths .* cos(angles));
-jac = [
-    zeros(n), eye(n);
-    dada, dadv
-    ];
+dAccelDVel = -2 * scaledMasses .* velocities .* sin(angles - angles.');
+dAccelDAng = scaledMasses .* (velocities.^2) .* cos(angles - angles.');
+dAccelDAng = dAccelDAng - diag(sum(dAccelDAng, 2) + cumulativeMasses * g .* lengths .* cos(angles));
+jac = [zeros(n), eye(n); dAccelDAng, dAccelDVel];
 
 end
 
