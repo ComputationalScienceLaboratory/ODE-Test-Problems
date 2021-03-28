b188b37fe69dd9fe811df56097744fae7dd172d9
Steven Roberts <sroberts994@gmail.com>
2021-03-28 16:43:07 -0400

    Accept nunber of bobs as argument

--- src/+otp/+pendulum/+presets/Canonical.m
+++ src/+otp/+pendulum/+presets/Canonical.m
@@ -1,12 +1,18 @@
 classdef Canonical < otp.pendulum.PendulumProblem
     methods
-        function obj = Canonical
-            params.g = otp.utils.PhysicalConstants.EarthGravity;
-            params.lengths = 1;
-            params.masses = 1;
+        function obj = Canonical(numBobs)
+            if nargin < 1
+                numBobs = 1;
+            end
             
-            y0 = [pi/3; 0];
-            tspan = [0 10];
+            params.g = otp.utils.PhysicalConstants.EarthGravity;
+            
+            o = ones(numBobs, 1);
+            params.lengths = o;
+            params.masses = o;
+            
+            y0 = [pi/2 * o; zeros(numBobs, 1)];
+            tspan = [0, 10];
             
             obj = obj@otp.pendulum.PendulumProblem(tspan, y0, params);
         end
