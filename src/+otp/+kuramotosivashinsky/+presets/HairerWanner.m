1b53abc4b0b19779860af8829665d19835a95477
Andrey A Popov <apopov@vt.edu>
2021-07-12 10:53:17 -0400

    Added the preset from Sol ODES2

---
+++ src/+otp/+kuramotosivashinsky/+presets/HairerWanner.m
@@ -0,0 +1,40 @@
+classdef HairerWanner < otp.kuramotosivashinsky.KuramotoSivashinskyProblem
+    
+    methods
+        function obj = HairerWanner(varargin)
+            
+            p = inputParser;
+            addParameter(p, 'Size', 128);
+            addParameter(p, 'L', 80*pi);
+
+            parse(p, varargin{:});
+            
+            s = p.Results;
+            
+            N = s.Size;
+            L = s.L;
+            
+            params.L = L;
+            
+            h = L/N;
+            
+            % exclude the left boundary point as it is identical to the
+            % right boundary point
+            x = linspace(h, L, N).';
+            
+            eta1 = min(x/L, 0.1 - x/L);
+            eta2 = 20*(x/L - 0.2).*(0.3 - x/L);
+            eta3 = min(x/L - 0.6, 0.7 - x/L);
+            eta4 = min(x/L - 0.9, 1 - x/L);
+            
+            u0 = 16*max(0, max(eta1, max(eta2, max(eta3, eta4))));
+            
+            u0hat = fft(u0);
+
+            tspan = [0, 150];
+            
+            obj = obj@otp.kuramotosivashinsky.KuramotoSivashinskyProblem(tspan, u0hat, params);
+            
+        end
+    end
+end
