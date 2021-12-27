b33374c2b652e2ec2098b49b49bf29301272f61f
Andrey A Popov <apopov@vt.edu>
2021-12-27 09:57:36 -0500

    Conversion of the Lorenz '63 problem to the new style defined by Steven

--- src/+otp/+lorenz63/+presets/Canonical.m
+++ src/+otp/+lorenz63/+presets/Canonical.m
@@ -1,36 +1,38 @@
 classdef Canonical < otp.lorenz63.Lorenz63Problem
-    % [Name]
-    %  Canonical
+    %CANONICAL This is the original problem presented in the literature.
+    % The initial condition is purposefully outside of the trapping region, but converges to it quite quickly.
+    %   Source:
+    %      Lorenz, E. N. (1963). Deterministic nonperiodic flow. Journal of atmospheric sciences, 20(2), 130-141.
     %
-    % [Description]
-    %  This is the original problem presented in the literature.
-    %  The initial condition is purposefully outside of the trapping region, but converges to it quite quickly.
-    %
-    % [NoVars]
-    %  3
-    %
-    % [Properties]
-    %  Chaotic
-    %
-    % [Usage]
-    %
+    %   See also otp.lorenz63.Lorenz63Problem
+
     methods
         function obj = Canonical(sigma, rho, beta)
-            if nargin < 1
+            %CANONICAL Construct a canonical Lorenz '63 problem
+            %   OBJ = CANONICAL(sigma, rho, beta) defines the Lorenz '63
+            %   problem with corresponding paramets
+            %
+            %   OBJ = CANONICAL() sets the default values of the parameters
+            %   to sigma = 10, rho = 28, and beta = 8/3. The arguments can
+            %   be left empty or ignored to use these defaults.
+    
+            if nargin < 1 || isempty(sigma)
                 sigma = 10;
             end
             
-            if nargin < 2
+            if nargin < 2 || isempty(rho)
                 rho = 28;
             end
             
-            if nargin < 3
+            if nargin < 3 || isempty(beta)
                 beta = 8/3;
             end
+
+            params = otp.lorenz63.Lorenz63Parameters;
             
-            params.sigma = sigma;
-            params.rho   = rho;
-            params.beta  = beta;
+            params.Sigma = sigma;
+            params.Rho   = rho;
+            params.Beta  = beta;
             
             y0    = [0; 1; 0];
             tspan = [0 60];
--- src/+otp/+lorenz63/+presets/Surprise.m
+++ src/+otp/+lorenz63/+presets/Surprise.m
@@ -1,29 +1,25 @@
 classdef Surprise < otp.lorenz63.Lorenz63Problem
-    % [Name]
-    %  Surprise
-    %
-    % [Description]
-    %  Strogatz's `surprise'
-    %
-    % [NoVars]
-    %  3
-    %
-    % [Properties]
-    %
-    % [Usage]
-    %
-    % [Citation]
-    %  Strogatz p 351
+    %SURPISE Strogatz's `surprise'
+    %    Source:
+    %      Strogatz, S. H. (2018). Nonlinear dynamics and chaos with student solutions manual: 
+    %          With applications to physics, biology, chemistry, and
+    %          engineering. CRC press. Page 351
     %
     methods
         function obj = Surprise
+            %SURPRISE Construct a surprise Lorenz '63 problem
+            %   OBJ = SURPRISE() defines the Lorenz '63
+            %   problem with corresponding parameters
+            %     sigma = 10, rho = 100, and beta = 8/3;
+            %
             sigma = 10;
             rho   = 100;
             beta  = 8/3;
 
-            params.sigma = sigma;
-            params.rho   = rho;
-            params.beta  = beta;
+            params = otp.lorenz63.Lorenz63Parameters;
+            params.Sigma = sigma;
+            params.Rho   = rho;
+            params.Beta  = beta;
             
             % Hand-picked initial conditions with the canonical timespan
             
