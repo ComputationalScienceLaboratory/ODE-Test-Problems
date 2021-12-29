538bb9259dd069e1640322f12c8c6c8845c96e73
Andrey A Popov <apopov@vt.edu>
2021-12-29 16:56:26 -0500

    Changed solver of nbody to default to ode15s as some of the systems are stiff

--- src/+otp/+nbody/NBodyProblem.m
+++ src/+otp/+nbody/NBodyProblem.m
@@ -26,7 +26,7 @@ classdef NBodyProblem < otp.Problem
         end
         
         function sol = internalSolve(obj, varargin)
-            sol = internalSolve@otp.Problem(obj, 'Method', @ode45, varargin{:});
+            sol = internalSolve@otp.Problem(obj, 'Method', @ode15s, varargin{:});
         end
         
         function mov = internalMovie(obj, t, y, varargin)
