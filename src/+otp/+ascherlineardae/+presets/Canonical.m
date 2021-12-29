387360250d26c2270a46081e2f1f8863fd5c7cca
Andrey A Popov <apopov@vt.edu>
2021-12-29 16:56:43 -0500

    Changed the Ascher linear DAE to be octave compatible

--- src/+otp/+ascherlineardae/+presets/Canonical.m
+++ src/+otp/+ascherlineardae/+presets/Canonical.m
@@ -1,4 +1,10 @@
 classdef Canonical < otp.ascherlineardae.AscherLinearDAEProblem
+    %CANONICAL The problem formulation of the linear DAE from the literature
+    %
+    % See
+    %    Ascher, Uri. "On symmetric schemes and differential-algebraic equations."
+    %    SIAM journal on scientific and statistical computing 10.5 (1989): 937-949.
+    
     methods
         function obj = Canonical(beta)
             tspan = [0.0; 1];
@@ -7,7 +13,8 @@ classdef Canonical < otp.ascherlineardae.AscherLinearDAEProblem
                 beta = 0.5;
             end
             
-            params.beta = beta;
+            params = otp.ascherlineardae.AscherLinearDAEParameters;
+            params.Beta = beta;
             
             y0 = [1; beta];
             
--- src/+otp/+ascherlineardae/AscherLinearDAEProblem.m
+++ src/+otp/+ascherlineardae/AscherLinearDAEProblem.m
@@ -1,9 +1,6 @@
 classdef AscherLinearDAEProblem < otp.Problem
-    %Index-1 DAE problem
-    % Problem description and fortran implementation can be found in
-    % Ascher, Uri. "On symmetric schemes and differential-algebraic equations."
-    % SIAM journal on scientific and statistical computing 10.5 (1989): 937-949.
-    
+    %ASCHERLINEARDAEPROBLEM This is an Index-1 DAE problem
+    %
     methods
         function obj = AscherLinearDAEProblem(timeSpan, y0, parameters)
             obj@otp.Problem('Ascher Linear DAE', 2, timeSpan, y0, parameters);
@@ -12,20 +9,13 @@ classdef AscherLinearDAEProblem < otp.Problem
     
     methods (Access = protected)
         function onSettingsChanged(obj)
-            beta = obj.Parameters.beta;
+            beta = obj.Parameters.Beta;
             
             obj.Rhs = otp.Rhs(@(t, y) otp.ascherlineardae.f(t, y, beta), ...
-                otp.Rhs.FieldNames.Jacobian, @(t, y) otp.ascherlineardae.jac(t, y, beta), ...
-                otp.Rhs.FieldNames.Mass, @(t) otp.ascherlineardae.mass(t, [], beta), ...
-                otp.Rhs.FieldNames.MStateDependence, 'none', ...
-                otp.Rhs.FieldNames.MassSingular, 'yes');
-        end
-        
-        function validateNewState(obj, newTimeSpan, newY0, newParameters)
-            validateNewState@otp.Problem(obj, newTimeSpan, newY0, newParameters)
-            
-            otp.utils.StructParser(newParameters) ...
-                .checkField('beta', 'scalar', 'real', 'finite');
+                'Jacobian', @(t, y) otp.ascherlineardae.jac(t, y, beta), ...
+                'Mass', @(t) otp.ascherlineardae.mass(t, [], beta), ...
+                'MStateDependence', 'none', ...
+                'MassSingular', 'yes');
         end
     end
 end
