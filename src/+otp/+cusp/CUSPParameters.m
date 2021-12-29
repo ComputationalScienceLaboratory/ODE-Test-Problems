43dddcf4e103dd7b80f87609cca751fb402a0255
Andrey A Popov <apopov@vt.edu>
2021-12-29 18:23:10 -0500

    CUSP octave fix

--- src/+otp/+cusp/CUSPParameters.m
+++ src/+otp/+cusp/CUSPParameters.m
@@ -3,11 +3,11 @@ classdef CUSPParameters
     %
     properties
         %SIZE is the desired size of the problem
-        Size (1,1) {mustBeInteger}
+        Size %MATLAB ONLY: (1,1) {mustBeInteger}
         %EPSILON is the stiffness parameter
-        Epsilon (1,1) {mustBePositive} = 1e-4
+        Epsilon %MATLAB ONLY: (1,1) {mustBePositive} = 1e-4
         %SIGMA is the diffusion coefficient
-        Sigma (1,1)  {mustBePositive} = 1/144
+        Sigma %MATLAB ONLY: (1,1)  {mustBePositive} = 1/144
     end
 end
 
