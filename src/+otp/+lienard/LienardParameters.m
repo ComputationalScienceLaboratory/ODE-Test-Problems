27d702079b86e95ddf81a6e144632f8f2d81d943
Andrey A Popov <apopov@vt.edu>
2021-12-30 11:39:19 -0500

    Fix for lineard problem

--- src/+otp/+lienard/LienardParameters.m
+++ src/+otp/+lienard/LienardParameters.m
@@ -3,16 +3,16 @@ classdef LienardParameters
     %
     properties
         %F is a function representing the non-linear damping
-        F {mustBeA(F, 'function_handle')} = @(x) 0
+        F %MATLAB ONLY: {mustBeA(F, 'function_handle')} = @(x) 0
         %DF is the derivative of F
-        DF {mustBeA(DF, 'function_handle')} = @(x) 0
+        DF %MATLAB ONLY: {mustBeA(DF, 'function_handle')} = @(x) 0
         %G is a function controlling the stiffness and restoring forces
-        G {mustBeA(G, 'function_handle')} = @(x) 0
+        G %MATLAB ONLY: {mustBeA(G, 'function_handle')} = @(x) 0
         %DG is the derivative of G
-        DG {mustBeA(DG, 'function_handle')} = @(x) 0
+        DG %MATLAB ONLY: {mustBeA(DG, 'function_handle')} = @(x) 0
         %P is usually a periodic funcgtion controlling the forcing
-        P {mustBeA(P, 'function_handle')} = @(t) 0
+        P %MATLAB ONLY: {mustBeA(P, 'function_handle')} = @(t) 0
         %DP is the derivative of P
-        DP {mustBeA(DP, 'function_handle')} = @(t) 0
+        DP %MATLAB ONLY: {mustBeA(DP, 'function_handle')} = @(t) 0
     end
 end
