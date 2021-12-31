1f7a58cb2a9c2f9de3cced4725794671d272c126
Andrey A Popov <apopov@vt.edu>
2021-12-31 09:00:28 -0500

    PR done

--- src/+otp/+protherorobinson/+presets/Canonical.m
+++ src/+otp/+protherorobinson/+presets/Canonical.m
@@ -1,17 +1,18 @@
 classdef Canonical < otp.protherorobinson.ProtheroRobinsonProblem
     methods
         function obj = Canonical(lambda, phi, dphi)
-            if nargin < 1
+            if nargin < 1 || isempty(lambda)
                 lambda = -1;
             end
-            if nargin < 2
+            if nargin < 2 || isempty(phi)
                 phi = @sin;
                 dphi = @cos;
             end
             
-            params.lambda = lambda;
-            params.phi = phi;
-            params.dphi = dphi;
+            params = otp.protherorobinson.ProtheroRobinsonParameters;
+            params.Lambda = lambda;
+            params.Phi = phi;
+            params.DPhi = dphi;
             tspan = [0, 10];
             y0 = phi(tspan(1));
             
--- src/+otp/+protherorobinson/ProtheroRobinsonProblem.m
+++ src/+otp/+protherorobinson/ProtheroRobinsonProblem.m
@@ -7,21 +7,12 @@ classdef ProtheroRobinsonProblem < otp.Problem
     
     methods (Access = protected)
         function onSettingsChanged(obj)
-            lambda = obj.Parameters.lambda;
-            phi = obj.Parameters.phi;
-            dphi = obj.Parameters.dphi;
+            lambda = obj.Parameters.Lambda;
+            phi = obj.Parameters.Phi;
+            dphi = obj.Parameters.DPhi;
             
             obj.Rhs = otp.Rhs(@(t, y) otp.protherorobinson.f(t, y, lambda, phi, dphi), ...
-                otp.Rhs.FieldNames.Jacobian, otp.protherorobinson.jac(lambda, phi, dphi));
-        end
-        
-        function validateNewState(obj, newTimeSpan, newY0, newParameters)
-            validateNewState@otp.Problem(obj, newTimeSpan, newY0, newParameters)
-            
-            otp.utils.StructParser(newParameters) ...
-                .checkField('lambda', 'finite', 'matrix', 'numeric') ...
-                .checkField('phi', 'function') ...
-                .checkField('dphi', 'function');
+                'Jacobian', otp.protherorobinson.jac(lambda, phi, dphi));
         end
     end
 end
