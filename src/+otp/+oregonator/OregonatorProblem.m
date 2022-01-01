b3af4af125086f3312a0f72f7de459060156a577
Steven Roberts <sroberts994@gmail.com>
2021-12-31 19:25:23 -0800

    Update Oregonator for octave

--- src/+otp/+oregonator/OregonatorProblem.m
+++ src/+otp/+oregonator/OregonatorProblem.m
@@ -8,15 +8,18 @@ classdef OregonatorProblem < otp.Problem
     methods (Access = protected)
         function onSettingsChanged(obj)
             obj.Rhs = otp.Rhs(@otp.oregonator.f, ...
-                otp.Rhs.FieldNames.Jacobian, @otp.oregonator.jac);
+                'Jacobian', @otp.oregonator.jac, ...
+                'Vectorized', 'on');
         end
         
         function fig = internalPlot(obj, t, y, varargin)
-            fig = internalPlot@otp.Problem(obj, t, y, 'yscale', 'log', varargin{:});
+            fig = internalPlot@otp.Problem(obj, t, y, ...
+                'yscale', 'log', varargin{:});
         end
         
         function mov = internalMovie(obj, t, y, varargin)
-            mov = internalMovie@otp.Problem(obj, t, y, 'yscale', 'log', varargin{:});
+            mov = internalMovie@otp.Problem(obj, t, y, ...
+                'yscale', 'log', varargin{:});
         end
     end
 end
