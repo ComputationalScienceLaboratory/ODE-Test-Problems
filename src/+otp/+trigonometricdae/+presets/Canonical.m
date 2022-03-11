295c19a1847ddbea3a473d881fbf3d7cce1b9f2f
Steven Roberts <sroberts994@gmail.com>
2022-03-10 20:05:42 -0800

    Add missing trig DAE parameters

--- src/+otp/+trigonometricdae/+presets/Canonical.m
+++ src/+otp/+trigonometricdae/+presets/Canonical.m
@@ -3,7 +3,9 @@ classdef Canonical < otp.trigonometricdae.TrigonometricDAEProblem
         function obj = Canonical()
             tspan = [0.5, 2];
             y0 = [sinh(0.5); tanh(0.5)];
-            obj = obj@otp.trigonometricdae.TrigonometricDAEProblem(tspan, y0, struct());            
+            params = otp.trigonometricdae.TrigonometricDAEParameters;
+            obj = obj@otp.trigonometricdae.TrigonometricDAEProblem(tspan, ...
+                y0, params);            
         end
     end
 end
