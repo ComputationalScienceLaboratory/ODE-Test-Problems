33b7f02e70629a88ec653c4160b0aa87f161c8bb
Steven Roberts <sroberts994@gmail.com>
2022-03-15 11:14:42 -0700

    Correct CUSP initial conditions

--- src/+otp/+cusp/+presets/Canonical.m
+++ src/+otp/+cusp/+presets/Canonical.m
@@ -6,32 +6,24 @@ classdef Canonical < otp.cusp.CUSPProblem
     methods
         function obj = Canonical(varargin)
             p = inputParser;
-            p.addParameter('Size', 32, @isscalar);
+            p.addParameter('Size', 32);
             p.addParameter('epsilon', 1e-4);
             p.addParameter('sigma', 1/144);
-            
             p.parse(varargin{:});
-            
-            s = p.Results;
-            
-            n = s.Size;
+            opts = p.Results;
             
             params = otp.cusp.CUSPParameters;
-            params.Size = n;
-            params.Epsilon = s.epsilon;
-            params.Sigma   = s.sigma;
+            params.Size = opts.Size;
+            params.Epsilon = opts.epsilon;
+            params.Sigma = opts.sigma;
             
-            xs = linspace(0, 1, n + 1)';
-            
-            % The last point is the same as the first, so we will ignore it.
-            xs = xs(1:(end-1));
-            
-            y0 = zeros(n, 1);
-            a0 = -2*cos(2*pi*xs);
-            b0 = -2*cos(2*pi*xs);
+            ang = 2 * pi / opts.Size * (1:opts.Size).';
+            y0 = zeros(opts.Size, 1);
+            a0 = -2*cos(ang);
+            b0 = 2*sin(ang);
 
             u0 = [y0; a0; b0];
-            tspan = [0 1.1];
+            tspan = [0; 1.1];
             
             obj = obj@otp.cusp.CUSPProblem(tspan, u0, params);
         end
