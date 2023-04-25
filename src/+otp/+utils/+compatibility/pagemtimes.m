d7080b5f843c550e0817889f77e72fee705bc5c1
Andrey A Popov <apopov@vt.edu>
2023-04-25 09:53:40 -0500

    significant upgrade to the pagemtimes stub

--- src/+otp/+utils/+compatibility/pagemtimes.m
+++ src/+otp/+utils/+compatibility/pagemtimes.m
@@ -1,14 +1,75 @@
-function x = pagemtimes(A, b)
+function X = pagemtimes(A, B, C, D)
 % OCTAVE FIX: This function is a naive implementation of the pagemtimes
 % functionality for the purposes of supporting octave and legacy matlab
 % installations
 
-N = size(A, 3);
+if nargin == 2
+    transpA = 'none';
+    transpB = 'none';
+elseif nargin == 4
+    transpA = B;
+    transpB = D;
+    B = C;
+else
+    error('OTP:invalidNumberOfArguments', 'The number of arguments has to be 2 or 4.');
+end
 
-x = zeros(size(A, 1), size(b, 2), N);
+if numel(size(A)) > 3 || numel(size(B)) > 3
+    error('OTP:notImplemented', 'Dimensions greater than 3 are not supported.');
+end
+
+% repmat A or B depending on the third dimension 
+N = max(size(A, 3), size(B, 3));
+
+if size(A, 3) == 1
+    repmat(A, 1, 1, N);
+elseif size(A, 3) ~= N
+    error('OTP:dimensionMismatch', 'The dimensions of A and B do not match.');
+end
+
+if size(B, 3) == 1
+    repmat(B, 1, 1, N);
+elseif size(B, 3) ~= N
+    error('OTP:dimensionMismatch', 'The dimensions of A and B do not match.');
+end
+
+% transpose A and B as required
+
+switch transpA 
+    case 'transpose'
+        Anew = zeros(size(A, 2), size(A, 1), N);
+        for i = 1:N
+            Anew(:, :, i) = A(:, :, i).';
+        end
+        A = Anew;
+    case 'ctranspose'
+        Anew = zeros(size(A, 2), size(A, 1), N);
+        for i = 1:N
+            Anew(:, :, i) = A(:, :, i)';
+        end
+        A = Anew;
+end
+
+switch transpB
+    case 'transpose'
+        Bnew = zeros(size(B, 2), size(B, 1), N);
+        for i = 1:N
+            Bnew(:, :, i) = B(:, :, i).';
+        end
+        B = Bnew;
+    case 'ctranspose'
+        Bnew = zeros(size(B, 2), size(B, 1), N);
+        for i = 1:N
+            Bnew(:, :, i) = B(:, :, i)';
+        end
+        B = Bnew;
+end
+
+
+X = zeros(size(A, 1), size(B, 2), N);
 
 for i = 1:N
-    x(:, :, i) = A(:, :, i)*b(:, :, i);
+    X(:, :, i) = A(:, :, i)*B(:, :, i);
 end
 
 end
