83a6fa5d174e3ce31b6809845f2d7eba9952b65d
Andrey A Popov <apopov@vt.edu>
2023-05-16 09:11:23 -0500

    simpler preset creator

---
+++ src/tests/getpresets.m
@@ -0,0 +1,41 @@
+function presets = getpresets()
+
+entry = "+otp/";
+
+if isempty(dir(entry))
+    entry = dir(which('otp.RHS'));
+    entry = entry.folder;
+end
+
+entry = fullfile(entry, '*', '+presets', '*.m');
+
+allpresets = dir(entry);
+
+presets = struct;
+
+for i = 1:numel(allpresets)
+
+[presets(i).presetclass, presets(i).name, presets(i).problem] = ...
+    constructPreset(allpresets(i)); 
+
+end
+
+end
+
+function [presetclass, presetname, problemname] = constructPreset(file)
+
+[~, presetname, ~] = fileparts(file.name);
+folders = strsplit(file.folder, filesep);
+problemname = folders{end-1};
+problemname = problemname(2:end);
+presetclass = sprintf('otp.%s.presets.%s', problemname, presetname);
+
+if strcmp(problemname, 'quasigeostrophic')
+    presetclass = sprintf('%s%s', presetclass, "('size', [16, 32])");
+end
+
+if strcmp(problemname, 'allencahn')
+    presetclass = sprintf('%s%s', presetclass, "('size', 16)");
+end
+
+end
