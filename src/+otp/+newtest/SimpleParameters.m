1539eaf4720172d7cdc77bef483d1a49ddbea3a4
Arash Sarshar <elswit@users.noreply.github.com>
2023-05-17 15:20:58 -0400

    Revert "dummy test problem adn instrauctions to add new test problems added"
    This reverts commit b761e4ec27da79f9f5dd550da5f64ad8872a751f.

--- src/+otp/+newtest/SimpleParameters.m
+++
@@ -1,6 +0,0 @@
-classdef SimpleParameters
-    %NewTestParameters
-    properties
-        param1 %MATLAB ONLY: (1,1) {mustBeNumeric, mustBeReal, mustBeNonnegative}
-    end
-end
--- src/+otp/+newtest/f.m
+++
@@ -1,3 +0,0 @@
-function dy = f(t, y, param1)
-  dy  = 1
-end
