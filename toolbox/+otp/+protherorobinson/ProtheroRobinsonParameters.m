4d4cb8f7a1ff79a0d11df0e707733b517ecc11d0
Arash Sarshar <elswit@users.noreply.github.com>
2023-07-08 18:32:59 -0400

    up PR-docs

--- toolbox/+otp/+protherorobinson/ProtheroRobinsonParameters.m
+++ toolbox/+otp/+protherorobinson/ProtheroRobinsonParameters.m
@@ -4,10 +4,10 @@ classdef ProtheroRobinsonParameters
         % Lambda ($\lambda$) is the eigenvalue of the problem.
         Lambda %MATLAB ONLY: (1,1) {mustBeFinite} = -1
 
-        % Phi is $\phi(t)$, a time-dependent function perturbing the right-hand-side funtion.
+        % Phi is $\phi(t)$, a time-dependent function perturbing the right-hand-side.
         Phi %MATLAB ONLY: {mustBeA(Phi, 'function_handle')} = @sin
 
-        %DPhi is ($\partial \phi / \partial t$),  the time-derivative of Phi.
+        %DPhi is ($\partial \phi / \partial t$), the time-derivative of Phi.
         DPhi %MATLAB ONLY: {mustBeA(DPhi, 'function_handle')} = @cos
     end
 end
