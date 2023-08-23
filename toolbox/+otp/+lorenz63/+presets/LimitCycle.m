6e31b922005213af5524b76f47441af7ec214dfc
Andrey A Popov <apopov@vt.edu>
2023-08-22 21:32:59 -0500

    cycleee

--- toolbox/+otp/+lorenz63/+presets/LimitCycle.m
+++ toolbox/+otp/+lorenz63/+presets/LimitCycle.m
@@ -1,5 +1,5 @@
 classdef LimitCycle < otp.lorenz63.Lorenz63Problem
-    % Lorenz '63 preset limit cycl, a non-chaotic preset, from :cite:p:`Str18` 
+    % Lorenz '63 preset limit cycle, a non-chaotic preset, from :cite:p:`Str18` 
     % which uses time span $t \in [0, 60]$, $\sigma = 10$, $\rho = 350$, 
     % $\beta = 8/3$, and intial conditions $y_0 = [0, 1, 0]^T$.
     
@@ -14,7 +14,7 @@ classdef LimitCycle < otp.lorenz63.Lorenz63Problem
             params.Rho   = rho;
             params.Beta  = beta;
             
-            % We will use Lorenz's initial conditions and timespan as Strogatz
+            % We use Lorenz's initial conditions and timespan as Strogatz
             % does not specify those in his book.
             
             y0    = [0; 1; 0];
