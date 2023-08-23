2e2fe3beb97956d9e5c1b4f2150cc11e2e857a48
Arash Sarshar <elswit@users.noreply.github.com>
2023-08-22 19:13:53 -0700

    patch for missing punctuation in robertson docs

--- toolbox/+otp/+robertson/RobertsonParameters.m
+++ toolbox/+otp/+robertson/RobertsonParameters.m
@@ -1,11 +1,11 @@
 classdef RobertsonParameters
     % Parameters for the Robertson problem.
     properties
-        % The reaction rate $K_1$
+        % The reaction rate $K_1$.
         K1 %MATLAB ONLY: (1, 1) {mustBeReal, mustBeNonnegative}
-        % The reaction rate $K_2$
+        % The reaction rate $K_2$.
         K2 %MATLAB ONLY: (1, 1) {mustBeReal, mustBeNonnegative}
-        % The reaction rate $K_3$
+        % The reaction rate $K_3$.
         K3 %MATLAB ONLY: (1, 1) {mustBeReal, mustBeNonnegative}
     end
 end
