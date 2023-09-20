5923c8b3bc35511878b17c510e9a02f49673c708
Andrey A Popov <apopov@vt.edu>
2023-09-19 21:20:42 -0500

    changed variable names to unicode

--- toolbox/+otp/+lorenz63/+presets/Canonical.m
+++ toolbox/+otp/+lorenz63/+presets/Canonical.m
@@ -1,7 +1,7 @@
 classdef Canonical < otp.lorenz63.Lorenz63Problem
     % Original Lorenz '63 preset presented in :cite:p:`Lor63`
-    % which uses time span $t \in [0, 60]$, $\sigma = 10$, $\rho = 28$, 
-    % $\beta = 8/3$, and intial conditions $y_0 = [0, 1, 0]^T$.
+    % which uses time span $t \in [0, 60]$, $σ = 10$, $ρ = 28$, 
+    % $β = 8/3$, and intial conditions $y_0 = [0, 1, 0]^T$.
 
     methods
         function obj = Canonical(varargin)
@@ -12,9 +12,9 @@ classdef Canonical < otp.lorenz63.Lorenz63Problem
             % varargin
             %    A variable number of name-value pairs. The accepted names are
             %
-            %    - ``sigma`` – Value of $\sigma$.
-            %    - ``rho`` – Value of $\rho$.
-            %    - ``beta`` – Value of $\beta$.
+            %    - ``sigma`` – Value of $σ$.
+            %    - ``rho`` – Value of $ρ$.
+            %    - ``beta`` – Value of $β$.
             %
 
             p = inputParser;
--- toolbox/+otp/+lorenz63/+presets/LimitCycle.m
+++ toolbox/+otp/+lorenz63/+presets/LimitCycle.m
@@ -1,13 +1,13 @@
 classdef LimitCycle < otp.lorenz63.Lorenz63Problem
     % Lorenz '63 preset limit cycle, a non-chaotic preset, from :cite:p:`Str18` 
-    % which uses time span $t \in [0, 60]$, $\sigma = 10$, $\rho = 350$, 
-    % $\beta = 8/3$, and intial conditions $y_0 = [0, 1, 0]^T$.
+    % which uses time span $t \in [0, 60]$, $σ = 10$, $ρ = 350$, 
+    % $β = 8/3$, and intial conditions $y_0 = [0, 1, 0]^T$.
     
     methods
         function obj = LimitCycle
             % Create the LimitCycle Lorenz '63 problem object.
             %
-            
+
             sigma = 10;
             rho   = 350;
             beta  = 8/3;
