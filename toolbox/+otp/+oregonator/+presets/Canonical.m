11190573db764c4307e11a902cb832d75d8e4991
Steven Roberts <sroberts994@gmail.com>
2023-09-30 12:03:45 -0700

    Start Oregonator docs

--- toolbox/+otp/+oregonator/+presets/Canonical.m
+++ toolbox/+otp/+oregonator/+presets/Canonical.m
@@ -1,9 +1,41 @@
 classdef Canonical < otp.oregonator.OregonatorProblem
+    % The Oregonator configuration used in :cite:p:`HW96` (p. 144) called OREGO. It uses time span $t \in [0, 360]$,
+    % initial condition $y_0 = [1, 2, 3]^T$, and parameters
+    %
+    % $$
+    % f &= 1, \\
+    % q &= 8.375 \cdot 10^{-6}, \\
+    % s &= 77.27, \\
+    % w &= 0.1610.
+    % $$
+
     methods
-        function obj = Canonical
+        function obj = Canonical(varargin)
+            % Create the Canonical problem object.
+            %
+            % Parameters
+            % ----------
+            %
+            % Returns
+            % -------
+            % obj : Canonical
+            %    The constructed problem.
+
+            p = inputParser;
+            p.addParameter('f', 1);
+            p.addParameter('q', 8.375e-6);
+            p.addParameter('s', 77.27);
+            p.addParameter('w', 0.1610);
+            p.parse(varargin{:});
+            opts = p.Results;
+
             tspan = [0, 360];
             y0 = [1; 2; 3];
             params = otp.oregonator.OregonatorParameters;
+            params.F = 1;
+            params.Q = 8.375e-6;
+            params.S = 77.27;
+            params.W = 0.1610;
             
             obj = obj@otp.oregonator.OregonatorProblem(tspan, y0, params);
         end
--- toolbox/+otp/+oregonator/OregonatorParameters.m
+++ toolbox/+otp/+oregonator/OregonatorParameters.m
@@ -1,4 +1,17 @@
 classdef OregonatorParameters
-    %OREGONATORPARAMETERS
+    % Parameters for the Oregonator problem.
+    properties
+        % The stoichiometric factor $f$.
+        F %MATLAB ONLY: (1, 1) {mustBeReal, mustBeFinite}
+        
+        % The reaction constant $q$.
+        Q %MATLAB ONLY: (1, 1) {mustBeReal, mustBeFinite}
+        
+        % The reaction constant $s$.
+        S %MATLAB ONLY: (1, 1) {mustBeReal, mustBeFinite}
+        
+        % The reaction constant $w$.
+        W %MATLAB ONLY: (1, 1) {mustBeReal, mustBeFinite}
+    end
 end
 
