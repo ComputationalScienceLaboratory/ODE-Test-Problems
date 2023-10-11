0af72b66e521859193ba5e49ec34e019ec417875
Steven Roberts <sroberts994@gmail.com>
2023-10-10 22:21:38 -0700

    Update PR

--- toolbox/+otp/+protherorobinson/+presets/Canonical.m
+++ toolbox/+otp/+protherorobinson/+presets/Canonical.m
@@ -8,37 +8,27 @@ classdef Canonical < otp.protherorobinson.ProtheroRobinsonProblem
     % $$
 
     methods
-        function obj = Canonical(lambda, phi, dphi)
+        function obj = Canonical(varargin)
             % Create the Canonical Prothero–Robinson problem object.
             %
             % Parameters
             % ----------
-            % 
-            % lambda : numeric(1, 1), default=-200
-            %    The stiffness parameter and eigenvalue of the Jacobian $\lambda$.
-            % phi : function_handle, default=@(t)10-(10+t).*exp(-t)
-            %    The exact solution.
-            % dphi : function_handle, default=@(t)(9+t).*exp(-t)
-            %    The time derivative of $\phi(t)$.
+            % varargin
+            %    A variable number of name-value pairs. The accepted names are
+            %
+            %    - ``Lambda`` – The stiffness parameter and eigenvalue of the Jacobian $\lambda$.
+            %    - ``Phi`` – The exact solution.
+            %    - ``DPhi`` – The time derivative of $\phi(t)$.
             %
             % Returns
             % -------
             % obj : ProtheroRobinsonProblem
             %    The constructed problem
-            if nargin < 1 || isempty(lambda)
-                lambda = -200;
-            end
-            if nargin < 2 || isempty(phi)
-                phi = @(t) 10 - (10 + t) .* exp(-t);
-                dphi = @(t) (9 + t) .* exp(-t);
-            end
             
-            params = otp.protherorobinson.ProtheroRobinsonParameters;
-            params.Lambda = lambda;
-            params.Phi = phi;
-            params.DPhi = dphi;
+            params = otp.protherorobinson.ProtheroRobinsonParameters('Lambda', -200, ...
+                'Phi', @(t) 10 - (10 + t) .* exp(-t), 'DPhi', @(t) (9 + t) .* exp(-t), varargin{:});
             tspan = [0, 15];
-            y0 = phi(tspan(1));
+            y0 = params.Phi(tspan(1));
             
             obj = obj@otp.protherorobinson.ProtheroRobinsonProblem(tspan, y0, params);
         end
--- toolbox/+otp/+protherorobinson/ProtheroRobinsonParameters.m
+++ toolbox/+otp/+protherorobinson/ProtheroRobinsonParameters.m
@@ -1,4 +1,4 @@
-classdef ProtheroRobinsonParameters
+classdef ProtheroRobinsonParameters < otp.Parameters
     % Parameters for the Prothero–Robinson problem.
     properties
         % The stiffness parameter and eigenvalue of the Jacobian $\lambda$.
@@ -9,5 +9,19 @@ classdef ProtheroRobinsonParameters
 
         % The time derivative of $\phi(t)$.
         DPhi %MATLAB ONLY: {mustBeA(DPhi, 'function_handle')} = @cos
+    end
+
+    methods
+        function obj = ProtheroRobinsonParameters(varargin)
+            % Create a Prothero–Robinson parameters object.
+            %
+            % Parameters
+            % ----------
+            % varargin
+            %    A variable number of name-value pairs. A name can be any property of this class, and the subsequent
+            %    value initializes that property.
+
+            obj = obj@otp.Parameters(varargin{:});
+        end
     end
 end
