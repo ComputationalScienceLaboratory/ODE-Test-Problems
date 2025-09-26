a22b10b31bfcde039dadff8eb390e26e41ce02ab
Andrey A Popov <apopov@vt.edu>
2025-09-26 12:27:30 -1000

    Updated Canonical

--- toolbox/+otp/+cr3bp/+presets/Canonical.m
+++ toolbox/+otp/+cr3bp/+presets/Canonical.m
@@ -1,12 +1,12 @@
 classdef Canonical < otp.cr3bp.CR3BPProblem
        methods
            function obj = Canonical(varargin)
-            % Create the NRHO CR3BP problem object.
-            % This is a trivial steady state orbit.
+            % Create the Canonical CR3BP problem object.
+            % This is a trivial steady state orbit oscillating around a lagrange point.
 
             mu = 0.5;
 
-            y0    = zeros(6, 1);
+            y0    = [0; 0; 0; 0; 0; 1];
             tspan = [0, 10];
             params = otp.cr3bp.CR3BPParameters('Mu', mu, 'SoftFactor', 1e-3, varargin{:});
             obj = obj@otp.cr3bp.CR3BPProblem(tspan, y0, params);            
